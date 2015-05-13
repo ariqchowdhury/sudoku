42a9c4b7d0c753dc9237671aafcbb9a550d86c08
Ariq Chowdhury <ariqchowdhury@gmail.com>
2015-05-12 21:44:08 -0700

    added method to set the state of a gameboard from a string

--- sudoku.cc
+++ sudoku.cc
@@ -1,5 +1,6 @@
 #include <vector>
 #include <iostream>
+#include <string>
 
 class Gameboard {
     int rows;
@@ -31,9 +32,30 @@ public:
         }
     }
 
+    // Format for initializer string will represent blank squares with spaces 
+    // and numbers side by side for adjacent spaces. Rows will also be laid out
+    // in one line. Chars will be used from the string until spaces are full.
+    // extra chars will be ignored, not enough chars will pad with blnks 
+    void set(std::string init) {
+        auto next_space = init.begin();
+
+        for (auto &r : spaces) {
+            for (auto &c : r) {
+                auto space = (*next_space) - '0';
+                if (space < 1 || space > 9) {
+                    c = 0; 
+                } else {
+                    c = space;
+                }
+                ++next_space;
+            } 
+        }
+    }
+
 };
 
 int main() {
     Gameboard gb(3, 3);
+    gb.set("123456789");
     gb.print();
 }
